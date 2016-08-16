29558089f2056a529682cb32698ab94d0d6b8108
Nathaniel Baer <njbbaer@gmail.com>
2016-08-16 15:41:50 -0700

    Refactor major automata types into subclasses

--- ca.py
+++ ca.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot, animation
 
 
 class Automata:
+
     def __init__(self, shape, density, neighborhood, rule):
         self.board = np.random.uniform(0, 1, shape)
         self.board = self.board < density
@@ -20,13 +21,7 @@ class Automata:
     def update(self, intervals=1):
         for i in range(intervals):
             convolution = Automata.fft_convolve2d(self.board, self.kernal)
-            shape = convolution.shape
-            new_board = np.zeros(shape)
-            new_board[np.where(np.in1d(convolution, self.rule[0]).reshape(shape)
-                               & (self.board == 1))] = 1
-            new_board[np.where(np.in1d(convolution, self.rule[1]).reshape(shape)
-                               & (self.board == 0))] = 1
-            self.board = new_board
+            self.apply_rule(convolution)
 
 
     def benchmark(self, interations):
@@ -61,63 +56,89 @@ class Automata:
         return convolution.round()
 
 
+class Life(Automata):
+
+    def __init__(self, shape, density, rule):
+        neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        super().__init__(shape, density, neighborhood, rule)
+
+
+    def apply_rule(self, convolution):
+        shape = convolution.shape
+        new_board = np.zeros(shape)
+        new_board[np.where(np.in1d(convolution, self.rule[0]).reshape(shape)
+                           & (self.board == 1))] = 1
+        new_board[np.where(np.in1d(convolution, self.rule[1]).reshape(shape)
+                           & (self.board == 0))] = 1
+        self.board = new_board
+
+
     @classmethod
     def conway(cls, shape, density):
-        neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        rule = [[2, 3], [3]]
-        automata = cls(shape, density, neighborhood, rule)
-        return automata
+        rule = ((2, 3), (3))
+        return cls(shape, density, rule)
 
 
     @classmethod
     def life34(cls, shape, density):
-        neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        rule = [[3, 4], [3, 4]]
-        automata = cls(shape, density, neighborhood, rule)
+        rule = ((3, 4), (3, 4))
+        automata = cls(shape, density, rule)
         return automata
 
 
     @classmethod
     def amoeba(cls, shape, density):
-        neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        rule = [[1, 3, 5, 8], [3, 5, 7]]
-        automata = cls(shape, density, neighborhood, rule)
-        return automata
+        rule = ((1, 3, 5, 8), (3, 5, 7))
+        return cls(shape, density, rule)
 
 
     @classmethod
     def anneal(cls, shape, density):
-        neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        rule = [[3, 5, 6, 7, 8], [4, 6, 7, 8]]
-        automata = cls(shape, density, neighborhood, rule)
-        return automata
+        rule = ((3, 5, 6, 7, 8), (4, 6, 7, 8))
+        return cls(shape, density, rule)
+
+
+class LargerThanLife(Automata):
+
+    def __init__(self, shape, density, rule, radius, use_center):
+        neighborhood = np.ones((radius*2+1, radius*2+1))
+        if not use_center:
+            neighborhood[radius][radius] = 0
+        super().__init__(shape, density, neighborhood, rule)
+
+
+    def apply_rule(self, convolution):
+        shape = convolution.shape
+        new_board = np.zeros(shape)
+        new_board[np.where((convolution >= self.rule[0][0]) & (convolution <= self.rule[0][1])
+                           & (self.board == 1))] = 1
+        new_board[np.where((convolution >= self.rule[1][0]) & (convolution <= self.rule[1][1])
+                           & (self.board == 0))] = 1
+        self.board = new_board
 
 
     @classmethod
     def bugs(cls, shape, density):
-        neighborhood = np.ones((11, 11))
-        rule = [np.arange(34, 59), np.arange(34, 46)]
-        automata = cls(shape, density, neighborhood, rule)
-        return automata
+        rule = ((34, 58), (34, 45))
+        radius = 5
+        return cls(shape, density, rule, radius, True)
 
 
     @classmethod
     def globe(cls, shape, density):
-        neighborhood = np.ones((17, 17))
-        neighborhood[8][8] = 0
-        rule = [np.arange(163, 224), np.arange(74, 253)]
-        automata = cls(shape, density, neighborhood, rule)
-        return automata
+        radius = 8
+        rule = ((163, 223), (74, 252))
+        return cls(shape, density, rule, radius, False)
 
 
 def main():
     # Create automata
-    automata = Automata.bugs((256, 256), density=0.5)
-    # automata = Automata.conway((256, 256), density=0.5)
-    # automata = Automata.life34((256, 256), density=0.12)
-    # automata = Automata.amoeba((256, 256), density=0.18)
-    # automata = Automata.anneal((256, 256), density=0.5)
-    # automata = Automata.globe((256, 256), density=0.4)
+    # automata = Life.conway((256, 256), 0.5)
+    # automata = Life.life34((256, 256), 0.12)
+    # automata = Life.amoeba((256, 256), 0.18)
+    # automata = Life.anneal((256, 256), 0.5)
+    automata = LargerThanLife.bugs((256, 256), 0.5)
+    # automata = LargerThanLife.globe((256, 256), 0.4)
 
     # Benchmark automata
     # automata.benchmark(interations=5000)
