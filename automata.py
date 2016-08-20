286a029702254c0e161dd91a47c7dfca5c45b114
Nathaniel Baer <njbbaer@gmail.com>
2016-08-20 01:44:25 -0700

    Refactor Automata without inheritance

--- automata.py
+++ automata.py
@@ -5,29 +5,56 @@ from matplotlib import pyplot, animation
 
 from rules import *
 
+# 
+
 
 class Automata:
 
-    def __init__(self, shape, density, neighborhood, rule):
-        self.board = np.random.uniform(0, 1, shape)
-        self.board = self.board < density
+    def __init__(self, board, neighborhood, rule):
+        self.board = board
+
+        neighborhood = np.array(neighborhood)
 
         n_height, n_width = neighborhood.shape
-        kernal = np.zeros(shape)
-        kernal[(shape[0] - n_height - 1) // 2 : (shape[0] + n_height) // 2,
-               (shape[1] - n_width - 1) // 2 : (shape[1] + n_width) // 2] = neighborhood
+        kernal = np.zeros(board.shape)
+        kernal[(board.shape[0] - n_height - 1) // 2 : (board.shape[0] + n_height) // 2,
+               (board.shape[1] - n_width - 1) // 2 : (board.shape[1] + n_width) // 2] = neighborhood
         self.kernal_ft = fft2(kernal)
 
-        self.rule = rule
+        get_ints = lambda x: [i for i in x if isinstance(i, int)]
+        self.rule_ints = (get_ints(rule[0]), get_ints(rule[1]))
+        get_ranges = lambda x: [i for i in x if not isinstance(i, int)]
+        self.rule_ranges = (get_ranges(rule[0]), get_ranges(rule[1]))
 
 
     def update(self, intervals=1):
         for i in range(intervals):
             convolution = self.fft_convolve2d()
-            self.apply_rule(convolution)
+            shape = convolution.shape
+            new_board = np.zeros(shape)
+
+
+            new_board[np.where(np.in1d(convolution, self.rule_ints[0]).reshape(shape)
+                           & (self.board == 1))] = 1
+            new_board[np.where(np.in1d(convolution, self.rule_ints[1]).reshape(shape)
+                           & (self.board == 0))] = 1
+            for rule_range in self.rule_ranges[0]:
+                new_board[np.where((convolution >= rule_range[0]) & (convolution <= rule_range[1])
+                                   & (self.board == 1))] = 1
+            for rule_range in self.rule_ranges[1]:
+                new_board[np.where((convolution >= rule_range[0]) & (convolution <= rule_range[1])
+                                   & (self.board == 0))] = 1
+
+
+
+            self.board = new_board
+
 
 
     def benchmark(self, interations):
+        """
+        XPS-15: Performed 5000 iterations of (256, 256) cells in 109.43707304 seconds
+        """
         start = time.process_time()
         self.update(interations)
         print("Performed", interations, "iterations of", self.board.shape, "cells in",
@@ -57,48 +84,26 @@ class Automata:
         return convolution.round()
 
 
-class Life(Automata):
-
-    def __init__(self, shape, density, rule):
-        neighborhood = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        super().__init__(shape, density, neighborhood, rule)
-
-
-    def apply_rule(self, convolution):
-        shape = convolution.shape
-        new_board = np.zeros(shape)
-        new_board[np.where(np.in1d(convolution, self.rule[0]).reshape(shape)
-                           & (self.board == 1))] = 1
-        new_board[np.where(np.in1d(convolution, self.rule[1]).reshape(shape)
-                           & (self.board == 0))] = 1
-        self.board = new_board
-
-
-class LargerThanLife(Automata):
-
-    def __init__(self, shape, density, rule, radius, include_center):
-        neighborhood = np.ones((radius*2+1, radius*2+1))
-        if not include_center:
-            neighborhood[radius][radius] = 0
-        super().__init__(shape, density, neighborhood, rule)
-
-
-    def apply_rule(self, convolution):
-        shape = convolution.shape
-        new_board = np.zeros(shape)
-        new_board[np.where((convolution >= self.rule[0][0]) & (convolution <= self.rule[0][1])
-                           & (self.board == 1))] = 1
-        new_board[np.where((convolution >= self.rule[1][0]) & (convolution <= self.rule[1][1])
-                           & (self.board == 0))] = 1
-        self.board = new_board
+def random_board(shape, density):
+    return np.random.uniform(0, 1, shape) < density
 
 
 def main():
-    shape = (256, 256)
+    bugs = Automata(
+        board = np.random.uniform(0, 1, (256, 256)) < 0.5,
+        neighborhood = np.ones((11, 11)),
+        rule = (((34, 58),), ((34, 45),))
+    )
 
-    # Bugs(shape).benchmark(interations=10000)
+    coagulations = Automata(
+        board = np.random.uniform(0, 1, (256, 256)) < 0.5,
+        neighborhood = ((1, 1, 1), (1, 0, 1), (1, 1, 1)),
+        rule = ((2, 3, 5, 6, 7, 8), (3, 7, 8))
+    )
 
-    Bugs(shape).animate(interval=100)
+    bugs.animate()
+
+    #coagulations.benchmark(interations=1000)
 
 
 if __name__ == "__main__":
