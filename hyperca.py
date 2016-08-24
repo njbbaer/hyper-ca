02c66a965b5d1cbf3bd7a85de263ee87684b5e1b
Nathaniel Baer <njbbaer@gmail.com>
2016-08-23 23:41:15 -0700

    Add larger tha life cli function

--- hyperca.py
+++ hyperca.py
@@ -1,9 +1,19 @@
+import sys
 import hyperca
+import numpy
 
-shape = (576, 1024)
 
-automata = hyperca.models.ltl534413458(shape)
+shape = (int(sys.argv[1]), int(sys.argv[2]))
 
-automata.populate(0.3)
 
-automata.animate()
+if str(sys.argv[3]) == "ltl":
+    radius = int(sys.argv[4])
+    neighborhood = numpy.ones((radius*2+1, radius*2+1))
+
+    birth_range = [int(sys.argv[5]), int(sys.argv[6])]
+    survival_range = [int(sys.argv[7]), int(sys.argv[8])]
+    rule = ([survival_range], [birth_range])
+
+    automata = hyperca.automata.Automata(shape, neighborhood, rule)
+    automata.populate(float(sys.argv[9]))
+    automata.animate()
